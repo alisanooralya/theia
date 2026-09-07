82f9e93b334a853364614e6adb6625c63501214b
alisanooralya <alisanooralya@gmail.com>
2026-09-07 15:18:06 +0800

    nothing

--- src/features/rpg/gacha-banner.js
+++ src/features/rpg/gacha-banner.js
@@ -210,7 +210,7 @@ function drawTitle(ctx, mx, era, name, subtitle) {
     titleSize -= 8;
     ctx.font = `bold ${titleSize}px ${SERIF}`;
   }
-  const titleY = 138;
+  const titleY = 142;
   ctx.fillStyle = 'rgba(10,4,20,0.7)';
   ctx.fillText(name, mx + 3, titleY + 3, nameMax);
 
