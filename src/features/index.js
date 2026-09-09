1b5f81f7ef6f0e9a14b13124dbd72fd42d674d1f
alisanooralya <alisanooralya@gmail.com>
2026-09-09 19:44:52 +0800

    remove: delete facebook and instagram downloader features

--- src/features/index.js
+++ src/features/index.js
@@ -1,4 +1,2 @@
 export { broadcastService } from './broadcast.js';
 export { downloaderService } from './downloader.js';
-export { instagramService } from './platforms/instagram.js';
-export { facebookService } from './platforms/facebook.js';
